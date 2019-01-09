6f5b050ae2e84078ea476c5589068de162e0bf6b
rbluckycs <rbluckycsedoc@gmail.com>
2019-01-09 16:58:02 +0530

    First File added

---
+++ DataStructure_CPP/Sample_Stack.cpp
@@ -0,0 +1,37 @@
+//#include<cstdio>
+#include<iostream>
+using namespace std;
+/*class Stack
+{
+//int data;
+Stack();
+Stack(int a)
+{
+int data;
+data=a;
+cout <<data;
+}
+
+};
+*/
+int main()
+{
+
+//Stack a=new Stack(12);
+
+//a.Stack(12);
+
+
+int a=26;
+char CH=254;
+
+cout << "Value of A=";
+cout << a ;
+cout << "\n";
+cout << "Value of A";
+cout << CH;
+
+
+return 0;
+}
+
